8dfa5f9a96e520bb282beba3c2b36348d5aae368
Jerome Etienne <jerome.etienne@gmail.com>
2018-12-17 12:17:09 +0000

    more work

---
+++ apps/model-predict/ui/ui-helpers.js
@@ -0,0 +1,62 @@
+
+////////////////////////////////////////////////////////////////////////
+//		Handle 'smoothedPrediction' event
+////////////////////////////////////////////////////////////////////////
+
+let bestClassesHistory = []
+let historyMaxLength = 1
+window.addEventListener('slouchingOrNotEvent', (domEvent) => {
+	let slouchingOrNotEvent = domEvent.detail
+
+	// keep only 'rawPrediction' event
+	if (slouchingOrNotEvent.type !== 'rawPrediction') return
+
+	// update bestClassesHistory
+	bestClassesHistory.push(slouchingOrNotEvent.rawBestClass)
+	while (bestClassesHistory.length > historyMaxLength) {
+		bestClassesHistory.shift()
+	}
+
+	// compute smoothedBestClass
+	let smoothedBestClass = 0
+	for (let i = 0; i < bestClassesHistory.length; i++) {
+		smoothedBestClass += bestClassesHistory[i]
+	}
+	smoothedBestClass /= bestClassesHistory.length
+	smoothedBestClass = Math.round(smoothedBestClass)
+
+	// update slouchingOrNotEvent
+	slouchingOrNotEvent.type = 'smoothedPrediction'
+	slouchingOrNotEvent.smoothedBestClass = smoothedBestClass
+
+	// forward the event
+	window.dispatchEvent(new CustomEvent("slouchingOrNotEvent", {
+		detail: slouchingOrNotEvent
+	}))
+})
+
+////////////////////////////////////////////////////////////////////////
+//		handle smoothedBestClassChange event
+////////////////////////////////////////////////////////////////////////
+
+let lastBestClass = null
+window.addEventListener('slouchingOrNotEvent', (domEvent) => {
+	let slouchingOrNotEvent = domEvent.detail
+	// keep only 'smoothedPrediction' event
+	if (slouchingOrNotEvent.type !== 'smoothedPrediction') return
+
+	// if smoothedBestClass is still the same, do nothing
+	if (slouchingOrNotEvent.smoothedBestClass === lastBestClass) return
+
+	// update lastBestClass
+	lastBestClass = slouchingOrNotEvent.smoothedBestClass
+
+	// update slouchingOrNotEvent
+	slouchingOrNotEvent.type = 'smoothedBestClassChange'
+
+	// forward the event
+	window.dispatchEvent(new CustomEvent("slouchingOrNotEvent", {
+		detail: slouchingOrNotEvent
+	}))
+})
+
---
+++ apps/model-predict/ui/ui-sounds.js
@@ -0,0 +1,93 @@
+import ModelConstants from '../../../src/model-constants.js'
+
+////////////////////////////////////////////////////////////////////////
+//		build dom elements
+////////////////////////////////////////////////////////////////////////
+
+document.querySelector('#predictUIPluginsContainerID').appendChild(createElementFromHTML(
+`<div>
+	<label>Sound Mute
+		<input type="checkbox" id="muteID" title='To mute the sound or not.' checked/>
+	</label>
+</div>`))
+
+function createElementFromHTML(htmlContent) {
+	var domElement = document.createElement('div');
+	domElement.innerHTML = htmlContent.trim();
+	return domElement.firstChild;
+}
+////////////////////////////////////////////////////////////////////////
+//		Play Sounds
+////////////////////////////////////////////////////////////////////////
+
+window.addEventListener('slouchingOrNotEvent', (domEvent) => {
+	let slouchingOrNotEvent = domEvent.detail
+	// keep only 'smoothedPrediction' event
+	if (slouchingOrNotEvent.type !== 'smoothedBestClassChange') return
+
+
+	if (slouchingOrNotEvent.smoothedBestClass === ModelConstants.CLASS_INDEXES.isSlouching) {
+		// playSound('predict-skins/sounds/265012__sethlind__toaster-oven-ding.wav', 0.05)
+		// playSound('predict-skins/sounds/341601__mike-stranks__gentle-stream.wav', 0.3)
+		playSound('predict-ui-plugins/sounds/351167__reitanna__that-s-bad.wav', 0.3)
+	} else if (slouchingOrNotEvent.smoothedBestClass === ModelConstants.CLASS_INDEXES.notSlouching) {
+		playSound('predict-ui-plugins/sounds/277021__sandermotions__applause-2.wav', 0.1)
+	} else {
+		console.assert(false, `unknown smoothBestClass ${slouchingOrNotEvent.smoothedBestClass}`)
+	}
+})
+
+
+////////////////////////////////////////////////////////////////////////
+//	handle notification sounds
+////////////////////////////////////////////////////////////////////////
+
+/**
+ * Audio domElement of the current playing sound. null if no sound is playing.
+ * @type {HTMLMediaElement}
+ */
+var notificationAudioEl = null
+
+/**
+ * Play a sound notification.
+ * 
+ * @param {string} soundURL the url pointing to the sound
+ * @param {Number} volume the volume of the sound between [0, 1]
+ */
+function playSound(soundURL, volume) {
+	// honor the muteID UI
+	let shouldBeMuted = document.querySelector('#muteID').checked ? true : false
+	if (shouldBeMuted === true) return
+
+	// stop currently playing sound if any
+	if (notificationAudioEl !== null) {
+		notificationAudioEl.pause()
+		notificationAudioEl = null
+	}
+
+	// create the new audio element 
+	var audioEl = new Audio(soundURL);
+	audioEl.volume = volume
+	audioEl.play();
+
+	// once the sound is ended, set notificationAudioEl to null
+	audioEl.addEventListener('ended', function () {
+		notificationAudioEl.pause()
+		notificationAudioEl = null
+	})
+
+	// update notificationAudioEl
+	notificationAudioEl = audioEl
+}
+
+////////////////////////////////////////////////////////////////////////
+//		Handle MuteID
+////////////////////////////////////////////////////////////////////////
+
+document.querySelector('#muteID').addEventListener('change', function () {
+	// if no sound is in progress, return now
+	if (notificationAudioEl === null) return
+
+	let shouldBeMuted = document.querySelector('#muteID').checked ? true : false
+	notificationAudioEl.muted = shouldBeMuted
+})
